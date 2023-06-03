9d7739dd558ab0fe574f112f0028564fb1eb6717
Alena Galysheva <ab7723@student.jamk.fi>
2023-06-03 13:53:38 +0300

    Exercises 2.11 completed

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({searchTerm,handleSearchChange}) => {
   return (
@@ -14,8 +15,8 @@ const Filter = ({searchTerm,handleSearchChange}) => {
 const PersonForm = ({addPerson, newName, handlePersonChange, newPhone, handlePhoneChange}) => {
   return (
     <form onSubmit={addPerson}>
-    <InputField name="name: " value={newName} onChange={handlePersonChange}/>
-    <InputField name="number: " value={newPhone} onChange={handlePhoneChange}/>
+    <InputField name="name" value={newName} onChange={handlePersonChange}/>
+    <InputField name="number" value={newPhone} onChange={handlePhoneChange}/>
     <div>
       <button type="submit">add</button>
     </div>
@@ -49,13 +50,16 @@ const PersonInfo = ({person}) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-  
+  const [persons, setPersons] = useState([])
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
+
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState('')
   const [searchTerm, setSearchTerm] = useState('')
