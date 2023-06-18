65a0fe49e489d55a4d813c288b898cd29421b612
Alena Galysheva <ab7723@student.jamk.fi>
2023-06-18 14:19:56 +0300

    Exercises 4.15-4.19 completed

--- part4/blog-list-backend/app.js
+++ part4/blog-list-backend/app.js
@@ -1,14 +1,19 @@
+const config = require("./utils/config")
 const express = require("express")
-require("express-async-errors")
 const app = express()
 const cors = require("cors")
-const blogsRouter = require("./controllers/blogs")
-const middleware = require("./utils/middleware")
-const config = require("./utils/config")
 const logger = require("./utils/logger")
 const mongoose = require("mongoose")
+require("express-async-errors")
+
+const blogsRouter = require("./controllers/blogs")
+const usersRouter = require("./controllers/users")
+const loginRouter = require("./controllers/login")
+
+const middleware = require("./utils/middleware")
 
 mongoose.set("strictQuery", false)
+
 logger.info("connecting to", config.MONGODB_URI)
 
 mongoose.connect(config.MONGODB_URI)
@@ -25,6 +30,8 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use("/api/blogs", blogsRouter)
+app.use("/api/users", usersRouter)
+app.use("/api/login", loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
--- part4/blog-list-backend/controllers/blogs.js
+++ part4/blog-list-backend/controllers/blogs.js
@@ -1,21 +1,55 @@
 const blogsRouter = require("express").Router()
+require("dotenv").config({ path: "../.env" })
 const Blog = require("../models/blog")
+const User = require("../models/user")
+const jwt = require("jsonwebtoken")
+
+const getTokenFrom = request => {
+	const authorization = request.get("authorization")
+	if (authorization && authorization.startsWith("Bearer ")) {
+		return authorization.replace("Bearer ", "")
+	}
+	return null
+}
 
 blogsRouter.get("/", async (request, response) => {
-	const blogs = await Blog.find({})
+	const blogs = await Blog
+		.find({}).populate("user", { username: 1, name: 1 })
+
 	response.json(blogs)
 })
   
 blogsRouter.post("/", async (request, response) => {
-	const { title, url } = request.body
+	const { title, author, url, likes } = request.body
 
 	if (!title || !url) {
 		return response.status(400).json({ error: "Title or URL is missing" })
 	}
 
-	const blog = new Blog(request.body)
+	const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+	if (!decodedToken.id) {
+		return response.status(401).json({ error: "token invalid" })
+	}
+  
+	const user = await User.findById(decodedToken.id)
+	// const user = await User.findOne({name: author})
+
+	if (!user) {
+		return response.status(400).json({ error: "User not found" })
+	}	
+
+	const blog = new Blog({
+		title: title,
+		author: user.name,
+		url: url,
+		likes: likes,
+		user: user._id
+	})
 
 	const result = await blog.save()
+	user.blogs = user.blogs.concat(result._id)
+	await user.save()
+
 	response.status(201).json(result)
 })
 
@@ -24,7 +58,7 @@ blogsRouter.delete("/:id", async (request, response) => {
 	response.status(204).end()
 })
 
-blogsRouter.put("/:id", async (request, response, next) => {
+blogsRouter.put("/:id", async (request, response) => {
 	const {likes} = request.body
 
 	const updatedBlog = await Blog.findByIdAndUpdate(
