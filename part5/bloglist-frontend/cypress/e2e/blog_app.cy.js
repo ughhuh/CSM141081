825172ede0385d485bbc56850cbb95a8c3f6c080
Alena Galysheva <ab7723@student.jamk.fi>
2023-07-08 13:41:34 +0300

    Exercise 5.17 completed

---
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -0,0 +1,25 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://example.cypress.io')
+  })
+})
+
+describe('Blog app', function() {
+  beforeEach(function() {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Matti Luukkainen',
+      username: 'mluukkai',
+      password: 'salainen'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
+    cy.visit('http://localhost:3000')
+  })
+
+  it('Login form is shown', function() {
+    cy.contains('log in to application')
+    cy.contains('username')
+    cy.contains('password')
+    cy.contains('login')
+  })
+})
